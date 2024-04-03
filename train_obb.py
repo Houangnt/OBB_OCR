3b0e52bed9fac60c1b771c7e85e0a4f2b0a19078
Houang869 <48252530+Houangnt@users.noreply.github.com>
2024-04-03 21:37:44 +0700

    Create train_obb.py

---
+++ train_obb.py
@@ -0,0 +1,7 @@
+rom ultralytics import YOLO
+
+# Load a model
+model = YOLO('ocr.pt')  # load a pretrained model (recommended for training)
+
+# Train the model
+results = model.train(data='data.yaml', epochs=100, imgsz=640)
